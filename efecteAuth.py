9c0e51d550d753e489ad6fa0b49e8bcb79f15c1f
cf-dave <david.weyenschops@collaboration-factory.de>
2022-02-02 14:56:12 +0100

    first results

--- efecteAuth.py
+++ efecteAuth.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 headers = {
     'accept': 'application/json',
@@ -20,11 +21,15 @@ headers = {
 }
 
 params = (
-    #('filter', '$status$ = \'01 - Not started\''),
+    #('filter', '$Self-Service Item$ = \'IT Repository | New\''),
     ('selectedAttributes', 'subject, description, status'),
     ('limit', '50'),
+    ('filterId', '1000000000'),
 )
 
 
-r2 = requests.get('https://cplace.efectecloud-test.com/rest-api/itsm/v1/dc/Incident/data/11063640', headers=headers)#, params=params)
-print(r2)
+r2 = requests.get('https://cplace.efectecloud-test.com/rest-api/itsm/v1/dc/ServiceRequest/data/11133725', headers=headers)#, params=params)
+print(r2.text)
+
+with open("result1.json", 'w') as fd:
+    json.dump(r2.text, fd)
