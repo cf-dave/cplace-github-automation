073b4d984db49e76f27a0351782d6cf5c51f9335
cf-dave <david.weyenschops@collaboration-factory.de>
2022-02-02 17:06:48 +0100

    Info extraction und js start via python

--- efecteAuth.py
+++ efecteAuth.py
@@ -1,5 +1,7 @@
+from asyncio import subprocess
 import requests
 import json
+from subprocess import check_output
 
 headers = {
     'accept': 'application/json',
@@ -29,7 +31,47 @@ params = (
 
 
 r2 = requests.get('https://cplace.efectecloud-test.com/rest-api/itsm/v1/dc/ServiceRequest/data/11133725', headers=headers)#, params=params)
-print(r2.text)
+#print(r2.text)
 
-with open("result1.json", 'w') as fd:
-    json.dump(r2.text, fd)
+#result = r2.text.replace("\\\"", "\"")
+
+ticket = json.loads(r2.text)
+serviceOffering = ticket['data']['ServiceOffering']['values'][0]['value']
+requestBundleName = ticket['data']['RequestBundleName']['values'][0]['value']
+serviceItem = ticket['data']['ServiceItem']['values'][0]['name']                    ## Hier steht die Anfrage Art drin
+additionalInformation = (ticket['data']['AdditionalInformation']['values'][0]['value']).split("\n")
+
+user, repo, level, justification = additionalInformation[0].split(':')[1], additionalInformation[1].split(':')[1],additionalInformation[2].split(':')[1],additionalInformation[3].split(':')[1]
+
+
+"""#Debug prints
+print(serviceOffering)
+print(requestBundleName)
+print(serviceItem)
+print(user)
+print(repo)
+print(level)
+print(justification)
+"""
+
+service = -1        ## Entscheidung welche Aktion ausgeführt wird
+
+if serviceItem == "IT Repository | New":
+    service = 1
+else:
+    service = 0
+
+if(service==1):
+    with open("todo.txt", 'w') as fd:
+        print(repo)
+        if(repo.startswith('cplace')):
+            fd.write("repoName:"+ repo)
+            p = check_output(['node', 'testJSOutput.js'])
+            print (p)
+        else:
+            #reject for naming scheme
+            print("Illegal name")
+
+
+#with open("result1.json", 'w') as fd:    
+    #fd.write(r2.text)
