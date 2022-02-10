4577ea7eb224145de96568a5291c722a3d35f12e
cf-dave <david.weyenschops@collaboration-factory.de>
2022-02-10 16:26:57 +0100

    Filterung nach Status Fortschritt

--- efecteAuth.py
+++ efecteAuth.py
@@ -10,7 +10,9 @@ from email.mime.text import MIMEText
 host = 'smtp.office365.com'
 port = 587
 
-smptObj = smtplib.SMTP([host])
+url = 'https://cplace.efectecloud-test.com/rest-api/itsm/v1/dc/ServiceRequest/data/'
+dc_url = 'https://cplace.efectecloud-test.com/itsm/EfecteFrameset.do#/workspace/datacard/view/'
+
 
 headers = {
     'accept': 'application/json',
@@ -33,10 +35,35 @@ headers = {
 
 params = (
     #('filter', '$Self-Service Item$ = \'IT Repository | New\''),
-    ('selectedAttributes', 'subject, request_service, status, ServiceItem'),
+    ('selectedAttributes', 'subject, request_service, status, ServiceItem, direktLink, AdditionalInformation'),
     ('limit', '50'),
     ('filterId', '1000000000'),
 )
+
+
+def sendEmail(link):
+    sender = 'david.weyenschops@collaboration-factory.de'
+    receivers = ['david.weyenschops@collaboration-factory.de']
+
+    text_subtype = 'plain'
+    content = 'The user ' + user+ ' wants to have ' + level.lower() + ' permission to the repo ' + repo +'. The justification for this action is \"' + justification+ '\". Please follow this link to approve or deny the request: ' + link
+
+    subject = "Sent from python"
+    
+    try:
+        smtpObj = smtplib.SMTP(host, port)
+        smtpObj.ehlo()
+        smtpObj.starttls()
+        smtpObj.ehlo()
+        smtpObj.login('david.weyenschops@collaboration-factory.de', 'DelfinFlosse45')
+        msg = MIMEText(content, text_subtype)
+        msg['Subject'] = subject
+        msg['From'] = sender
+        smtpObj.sendmail(sender,receivers,msg.as_string())
+        print("Success")
+        smtpObj.quit()
+    except:
+        print("Error")
 
 
 r2 = requests.get('https://cplace.efectecloud-test.com/rest-api/itsm/v1/dc/ServiceRequest/data/12204626', headers=headers)#, params=params) #||||11133725
@@ -45,29 +72,81 @@ r3 = requests.get('https://cplace.efectecloud-test.com/rest-api/itsm/v1/dc/Servi
 r2T = json.loads(r2.text)
 r3T = json.loads(r3.text)
 dataCards = r3T['data']
+#print(r2T)
 
+notStarted = []
+approved = []
 for datacard in dataCards:     #Go through and filter the git hub ones out
-    notStarted = []
-    approved = []
+
     if len(datacard['data']['request_service']['values']) > 0:
         if datacard['data']['request_service']['values'][0]['name'] == 'GitHub Management':
             service = datacard['data']['ServiceItem']['values'][0]['name']
             status = datacard['data']['status']['values'][0]['value']
             id = datacard['dataCardId']
             if status == '01 - Not started':
+                print(status)
                 notStarted.append([id, service])
             elif status == '03 - Approved' :
+                print(status)
                 approved.append([id, service])
             else:
                 continue
 ghRequests = [notStarted, approved]
 
+print(len(notStarted))
+
+for ticket in notStarted:
+    response = requests.get(url+str(ticket[0]), headers=headers, params=params)
+    text = json.loads(response.text)
+    print(text)
+    serviceOffering = text['data']['ServiceOffering']['values'][0]['value']
+    serviceItem = text['data']['ServiceItem']['values'][0]['name']     
+    #link = text['data']['direktLink']['values'][0]['value']
+    additionalInformation = (text['data']['AdditionalInformation']['values'][0]['value']).split(" ")
+    #print(additionalInformation)
+    user = "cf-dave"
+    repo, level, justification = additionalInformation[1].split(':')[1], additionalInformation[3].split(':')[1],additionalInformation[4].split(':')[1]#,additionalInformation[3].split(':')[1]
+    if level == "Read":
+        level = "pull"
+    elif level == "Write":
+        level = "push"
+    link = dc_url + ticket[0]
+
+    service = -1        ## Entscheidung welche Aktion ausgeführt wird
+
+    if serviceItem == "IT Repository | New":
+        service = 1
+    elif serviceItem == "IT Repository | Access Request":
+        service = 2
+    else:
+        service = 0
+
+    if service==1:
+        with open("todo.txt", 'w') as fd:
+            print(repo)
+            if(repo.startswith('cplace')):
+                fd.write("repoName:"+ repo)
+                p = check_output(['node', 'createRepo.js'])
+                if p == "Script ran through":
+                    print("Script ran through")
+            else:
+                #reject for naming scheme
+                print("Illegal name")
+    elif service == 2:
+        with open("todo.txt", 'w') as fd:
+                #print(repo)
+                fd.write("user:"+ user+"\n")
+                #if(repo.startswith('cplace')):
+                fd.write("repoName:"+ repo+"\n")
+                fd.write("level:"+level+"\n")
+                fd.write("justification:"+justification+"\n")  
+    sendEmail(link)
 
 
-ticket = json.loads(r2.text)
+
+"""
 status = ticket['data']['status']['values'][0]['value']
 serviceOffering = ticket['data']['ServiceOffering']['values'][0]['value']
-#requestBundleName = ticket['data']['RequestBundleName']['values'][0]['value']
 serviceItem = ticket['data']['ServiceItem']['values'][0]['name']                    ## Hier steht die Anfrage Art drin
 additionalInformation = (ticket['data']['AdditionalInformation']['values'][0]['value']).split("\n")
 print(status)
@@ -80,7 +159,7 @@ if level == "Read":
 elif level == "Write":
     level = "push"
 
-"""#Debug prints
+#Debug prints
 print(serviceOffering)
 #print(requestBundleName)
 print(serviceItem)
@@ -88,7 +167,7 @@ print(user)
 print(repo)
 print(level)
 print(justification)
-"""
+
 
 service = -1        ## Entscheidung welche Aktion ausgeführt wird
 
@@ -119,7 +198,7 @@ elif service == 2:
             fd.write("level:"+level+"\n")
             fd.write("justification:"+justification+"\n")  
     if(status == "01 - Not started"):
-        """
+
         outlook = win32com.client.Dispatch('outlook.application')
         mail = outlook.CreateItem(0)
         mail.To = 'david.weyenschops@collaboration-factory.de'
@@ -132,48 +211,11 @@ elif service == 2:
         mail.Send()
             #email out
         """
-        sender = 'david.weyenschops@collaboration-factory.de'
-        receivers = ['david.weyenschops@collaboration-factory.de']
-        message = "Lol"
-
-        text_subtype = 'plain'
-        content = """\
-            'The user ' + user+ ' wants to have ' + level.lower() + ' permission to the repo ' + repo +'. The justification for this action is \"' + justification+ '\". Please follow this link to approve or deny the request. https://www.youtube.com/watch?v=dQw4w9WgXcQ'
-            """
-        subject = "Sent from python"
-        try:
-            smtpObj = smtplib.SMTP(host, port)
-            smtpObj.ehlo()
-            smtpObj.starttls()
-            smtpObj.ehlo()
-            smtpObj.login('david.weyenschops@collaboration-factory.de', 'DelfinFlosse45')
-            msg = MIMEText(content, text_subtype)
-            msg['Subject'] = subject
-            msg['From'] = sender
-            smtpObj.sendmail(sender,receivers,msg.as_string())
-            print("Success")
-            smtpObj.quit()
-        except:
-            print("Error")
-    elif(status == "03 - Approved"):
-        #start js script
-        p = check_output(['node', 'AddUserToRepo.js'])
-        if p == "Script ran through":
-            print("addToRepo succesful")
-    else:
-        ("Illegal status")
+        
+if(status == "03 - Approved"):
+    #start js script
+    p = check_output(['node', 'AddUserToRepo.js'])
+    if p == "Script ran through":
+        print("addToRepo succesful")
 else:
-    print("Illegal request")
-
-"""
-mail = outlook.CreateItem(0)
-
-    mail.To = 'david.weyenschops@collaboration-factory.de'
-    mail.Subject = 'GitHub Access Approval'
-    mail.HTMLBody = '<h3>Please follow this link. https://www.youtube.com/watch?v=dQw4w9WgXcQ</h3>'
-    mail.Body = "Please follow this link. https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-    #mail.Attachments.Add('c:\\sample.xlsx')
-    #mail.Attachments.Add('c:\\sample2.xlsx')
-    #mail.CC = 'somebody@company.com'
-    mail.Send()
-"""
+    ("Illegal status")
